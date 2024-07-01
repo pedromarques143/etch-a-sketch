a27a05b5a60db048fce1937072a7fde5ee92c9cb
pedromarques143 <pedromarques.trad@gmail.com>
2024-07-01 14:56:45 +0100

    Created squareHover function

--- script.js
+++ script.js
@@ -4,10 +4,22 @@ let containerSize = 16;
 
 for (let x = 0; x < containerSize; x++) {
     let singleLine = document.createElement("div");
-    squareContainer.appendChild(singleLine);   
+    squareContainer.appendChild(singleLine);
+
     for (let i = 0; i < containerSize; i++) {
         let singleSquare = document.createElement("div");
-        singleSquare.textContent = "o";
+        singleSquare.style.width = "20px";
+        singleSquare.style.height = "20px";
+        singleSquare.style.borderStyle = "solid";
+        singleSquare.style.borderWidth = "1px"
         singleLine.appendChild(singleSquare);
+        singleSquare.className = "single-square";
+        squareHover(singleSquare);
     }
 }
+
+function squareHover(element) {
+    element.addEventListener("mouseover", () => {
+        element.style.backgroundColor = "red";
+    });
+}  
