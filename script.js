5ccef6e7d16a01d39ecd477e7147f7d8cabaed8b
pedromarques143 <pedromarques.trad@gmail.com>
2024-07-03 00:57:25 +0100

    Created boardReset function

--- script.js
+++ script.js
@@ -1,26 +1,73 @@
 const squareContainer = document.querySelector(".square-container");
 
-let containerSize = 16;
-let selectedMode = document.querySelector("input[type=radio][name=selected-mode]:checked").id;
+let containerSize;
 
 
-for (let x = 0; x < containerSize; x++) {
-    let singleLine = document.createElement("div");
-    squareContainer.appendChild(singleLine);
+let selectedMode = "click-mode";
+let allModesArr = document.querySelectorAll("input[name='selected-mode']");
 
-    for (let i = 0; i < containerSize; i++) {
-        let singleSquare = document.createElement("div");
-        singleSquare.style.width = "20px";
-        singleSquare.style.height = "20px";
-        singleSquare.style.borderStyle = "solid";
-        singleSquare.style.borderWidth = "1px"
-        singleSquare.style.marginTop = "-1px";
-        singleSquare.style.marginLeft = "-1px";
-        singleLine.appendChild(singleSquare);
-        singleSquare.className = "single-square";
-        squareHover(singleSquare);
+for (const mode of allModesArr) {
+    mode.addEventListener("change", () => {
+        selectedMode = mode.id;
+    })    
+};
+
+//default size
+createBoard(16);
+boardReset();
+
+
+
+
+
+function createBoard(number) {
+    let squareSize = 500 / number;
+        for (let x = 0; x < number; x++) {
+            let singleLine = document.createElement("div");
+            squareContainer.appendChild(singleLine);
+        
+            for (let i = 0; i < number; i++) {
+                let singleSquare = document.createElement("div");
+                singleSquare.className = "single-square"; 
+                singleSquare.style.width = `${squareSize - 1}px`;
+                singleSquare.style.height = `${squareSize - 1}px`;
+                singleSquare.style.borderStyle = "solid";
+                singleSquare.style.borderWidth = "1px"
+                singleSquare.style.marginTop = "-1px";
+                singleSquare.style.marginLeft = "-1px";
+                singleLine.appendChild(singleSquare);                     
+            }
+        }
+    let allSquaresArr = document.querySelectorAll(".single-square");
+
+    for (let square of allSquaresArr) {
+        if (selectedMode == "click-mode") {
+            squareClick(square);
+        } else if (selectedMode == "hover-mode") {
+            squareHover(square);
+        }
     }
 }
+
+
+function boardReset() {
+    const changeForm = document.querySelector(".size-form");
+    changeForm.addEventListener("submit", () => {
+        event.preventDefault(); 
+        
+        let input = document.getElementById("square-size");
+        let inputValue = Number(input.value);
+
+        if (inputValue <= 100 && inputValue > 0 && typeof inputValue == "number") {
+            containerSize = inputValue;
+            squareContainer.innerHTML = "";
+            createBoard(containerSize);                
+        } else {
+            alert("Enter a number between 1 and 100!");   
+        }       
+    });
+}
+
 
 function squareHover(element) {
     element.addEventListener("mouseover", () => {
@@ -33,3 +80,7 @@ function squareClick(element) {
         element.style.backgroundColor = "black";
     });
 }
+
+
+//TO DO
+//add color selection
