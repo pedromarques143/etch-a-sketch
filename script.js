7a42ebdadb5cf3d0c3b51bd94e9e1670235a7411
pedromarques143 <pedromarques.trad@gmail.com>
2024-07-03 01:57:28 +0100

    Fixed squareHover and squareClick functions

--- script.js
+++ script.js
@@ -1,50 +1,55 @@
 const squareContainer = document.querySelector(".square-container");
 
 let containerSize;
-
-
-let selectedMode = "click-mode";
 let allModesArr = document.querySelectorAll("input[name='selected-mode']");
 
-for (const mode of allModesArr) {
-    mode.addEventListener("change", () => {
-        selectedMode = mode.id;
-    })    
-};
+let stopClick = false;
+let stopHover = true;
 
 //default size
 createBoard(16);
 boardReset();
 
+//color pallete
 
-
-
-
+//function declarations
 function createBoard(number) {
     let squareSize = 500 / number;
-        for (let x = 0; x < number; x++) {
-            let singleLine = document.createElement("div");
-            squareContainer.appendChild(singleLine);
+    for (let x = 0; x < number; x++) {
+        let singleLine = document.createElement("div");
+        squareContainer.appendChild(singleLine);
         
-            for (let i = 0; i < number; i++) {
-                let singleSquare = document.createElement("div");
-                singleSquare.className = "single-square"; 
-                singleSquare.style.width = `${squareSize - 1}px`;
-                singleSquare.style.height = `${squareSize - 1}px`;
-                singleSquare.style.borderStyle = "solid";
-                singleSquare.style.borderWidth = "1px"
-                singleSquare.style.marginTop = "-1px";
-                singleSquare.style.marginLeft = "-1px";
-                singleLine.appendChild(singleSquare);                     
-            }
+        for (let i = 0; i < number; i++) {
+            let singleSquare = document.createElement("div");
+            singleSquare.className = "single-square"; 
+            singleSquare.style.width = `${squareSize - 1}px`;
+            singleSquare.style.height = `${squareSize - 1}px`;
+            singleSquare.style.borderStyle = "solid";
+            singleSquare.style.borderWidth = "1px"
+            singleSquare.style.marginTop = "-1px";
+            singleSquare.style.marginLeft = "-1px";
+            singleLine.appendChild(singleSquare);                     
         }
+    }
     let allSquaresArr = document.querySelectorAll(".single-square");
 
     for (let square of allSquaresArr) {
-        if (selectedMode == "click-mode") {
-            squareClick(square);
-        } else if (selectedMode == "hover-mode") {
-            squareHover(square);
+        squareClick(square);
+        
+        let radioButtons = document.querySelectorAll("input[type='radio']");
+
+        for (let radio of radioButtons) {
+            radio.addEventListener("change", () => {
+                if (radio.id == "click-mode") {
+                    stopClick = false;
+                    stopHover = true;
+                    squareClick(square);                    
+                } else {
+                    stopClick = true;
+                    stopHover = false;
+                    squareHover(square);
+                }
+            });
         }
     }
 }
@@ -69,17 +74,25 @@ function boardReset() {
 }
 
 
-function squareHover(element) {
-    element.addEventListener("mouseover", () => {
-        element.style.backgroundColor = "black";
+function squareClick(element) {
+    element.addEventListener("click", () => {
+        if (!stopClick) {
+            element.style.backgroundColor = "black";
+        }
     });
 }
 
-function squareClick(element) {
-    element.addEventListener("click", () => {
-        element.style.backgroundColor = "black";
+
+function squareHover(element) {
+    element.addEventListener("mouseover", () => {
+        if (!stopHover) {
+            element.style.backgroundColor = "black";
+        }
     });
 }
+
+
+
 
 
 //TO DO
